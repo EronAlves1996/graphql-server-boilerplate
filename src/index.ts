20321acd4268b92c33e58cb73644aab196441d75
Eron Alves <eron.alves@rocketmail.com>
2023-05-01 15:14:09 -0300

    adjust node types

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import fastify from "fastify";
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  sendResult,
+  shouldRenderGraphiQL,
+} from "graphql-helix";
+
+const app = fastify({ logger: true });
+
+app.all("/graphql", async (req, res) => {
+  const request = {
+    body: req.body,
+    headers: req.headers,
+    method: req.method,
+    query: req.query,
+  };
+
+  // Determine whether we should render GraphiQL instead of returning an API response
+  if (shouldRenderGraphiQL(request)) {
+    res.send(renderGraphiQL());
+  } else {
+    // Extract the Graphql parameters from the request
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    // Validate and execute the query
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema,
+    });
+
+    // processRequest returns one of three types of results depending on how the server should respond
+    // 1) RESPONSE: a regular JSON payload
+    // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
+    // 3) PUSH: a stream of events to push back down the client for a subscription
+    // The "sendResult" is a NodeJS-only shortcut for handling all possible types of Graphql responses,
+    // See "Advanced Usage" below for more details and customizations available on that layer.
+    sendResult(result, res.raw);
+  }
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
